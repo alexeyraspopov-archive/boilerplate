276de1e41c84eb4816737ed47331048e4e89545d
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-04-16 02:09:46 +0300

    added .editorconfig

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-	render() {
-		return <h1>Hello, World!</h1>;
-	}
+  render() {
+    return <h1>Hello, World!</h1>;
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('main'));
