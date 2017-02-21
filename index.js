58782757a58eb8a12aaa501a0fdc95e601d46da9
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-02-21 19:10:31 +0200

    move stuff to modules/ folder

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return <h1>Hello, World!</h1>;
-}
+import App from './modules/App';
 
 ReactDOM.render(<App />, document.querySelector('main'));
